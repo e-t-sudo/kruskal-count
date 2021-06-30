fb2180e8349288bb2bad1106cfecdb7eb7b37e9b
e_t <77915530+e-t-sudo@users.noreply.github.com>
2021-06-30 16:35:55 +0530

    Flipping the predicted card

--- index.js
+++ index.js
@@ -2,14 +2,15 @@
     let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]; //"Ace to King"
     let div = document.getElementById("cards");
     let orderedDeck = [];
+    let deck = [];
     for(let i=0; i<suits.length; i++){
         for(let j=0; j<values.length; j++){
-            orderedDeck.push(values[j]+suits[i]);
+            orderedDeck.push(values[j]+","+suits[i]);
         }
     }
     let shuffledDeck = shuffleArray(orderedDeck);
     for(let i=0; i<shuffledDeck.length; i++){
-        let card_name = shuffledDeck[i];
+        let card_name = shuffledDeck[i].split(",").join("");
         let card_img = "./cards/"+card_name+".png";
         let funName = "turnCard(\""+card_name+"\")";
         div.innerHTML += "<div onClick="+funName+" id="+card_name+" back = '1' class='card'>"+card_name+"</div>";
@@ -36,3 +37,32 @@
         }
         return array;
     }
+    let jumps = {
+        "1": 1,
+        "2": 2,
+        "3": 3,
+        "4": 4,
+        "5": 5,
+        "6": 6,
+        "7": 7,
+        "8": 8,
+        "9": 9,
+        "10": 10,
+        "11": 1,
+        "12": 1,
+        "13": 1
+    }
+    function flipPrediction(){
+        let numericalValue = values.indexOf(shuffledDeck[0].split(",")[0])+1;
+        let jumpValue = jumps[numericalValue];
+        x = jumpValue;
+        while(x<=51){
+            console.log(x);
+            finalIndex = x;
+            x = x + jumps[values.indexOf(shuffledDeck[x].split(",")[0])+1];
+        }
+        let predCard = shuffledDeck[finalIndex].split(",").join("");
+        document.getElementById("prediction").setAttribute("href", "#"+predCard)
+        turnCard(predCard);
+    }
+    flipPrediction();
